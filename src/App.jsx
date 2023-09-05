28eb18e7a9b0c1ae8148c595e5d2d4e93a131142
SsimbwaJoshua <ssimbwajoshua124@gmail.com>
2023-09-05 10:52:01 +0300

    added countries list

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
+import CountryList from "./components/CountryList";
 
 const BASE_URL = "http://localhost:9000";
 
@@ -45,6 +46,10 @@ function App() {
           <Route
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route
+            path="countries"
+            element={<CountryList cities={cities} isLoading={isLoading} />}
           />
           <Route path="countries" element={<p>countries</p>} />
           <Route path="form" element={<p>form</p>} />
---
+++ src/components/CountryList.jsx
@@ -0,0 +1,29 @@
+import CountryItem from "./CountryItem";
+import styles from "./CountryList.module.css";
+import Spinner from "./Spinner";
+import Message from "./Message";
+
+function CountryList({ isLoading, cities }) {
+  if (isLoading) return <Spinner />;
+  if (!cities.length)
+    return (
+      <Message
+        message={"Add your first city by clicking on a city on the map"}
+      />
+    );
+
+  const countries = cities.reducer((arr, city) => {
+    if (arr.map((el) => el.country).includes(city.country)) {
+      return [...arr, { country: city.country, emoji: city.emoji }];
+    } else return arr;
+  }, []);
+  return (
+    <ul className={styles.countryList}>
+      {countries.map((country) => (
+        <CountryItem city={country} />
+      ))}
+    </ul>
+  );
+}
+
+export default CountryList;
